e06d578d635fb8d6e38f66a5a027f94344a1e42a
MarcellKov <kmarcell379@gmail.com>
2024-03-21 21:56:35 +0100

    card-stop

--- src/app/comps/cards.js
+++ src/app/comps/cards.js
@@ -1,14 +1,48 @@
 import { useEffect, useState } from "react"
 export function Card(props) {
     const [data, setData] = useState(0)
+    var flag=false
 
     useEffect(() => { const iv = setInterval(() => {
+     if (flag==true){ console.log("leall");return}
      if (data == props.list.length - 1) { setData(0) } else { setData(data + 1) } 
     }, 2000); return () => clearInterval(iv) }, [data])
 
+    function Move(){
+        if (data==props.list.length-1){setData(0)} else {setData(data+1)}
+        console.log(data)
+    }
+
+    function True(){
+        flag=true
+    }
+    function False(){
+        flag=false
+    }
+
+
     return (
         <>
-            <p className={`opacity-70 transition-opacity duration-500 ease-in-out hover:opacity-100`}>{props.list[data]}</p>
+        <div className="flex items-center h-[100%] justify-evenly ">
+
+            <div className="w-[33%]" >
+            <svg width="100%" height="40" onClick={Move} onMouseEnter={True} onMouseLeave={False}>
+            <circle cx={"50%"} cy={"50%"} r="20" fill="blue"/>
+            </svg>
+            </div>
+
+            <div className="w-[33%]">
+            <p className={`opacity-70 transition-opacity duration-500 ease-in-out hover:opacity-100 left-[33%]`}>{props.list[data]}</p>
+            </div>
+
+            <div className="w-[33%]">
+            <svg width="100%" height="40" onClick={Move} onMouseEnter={True} onMouseLeave={False}>
+            <circle cx={"50%"} cy={"50%"} r="20" fill="blue"/>
+            </svg>
+            </div>
+            
+
+        </div>
         </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
-
+import { Card } from "./comps/cards";
 export default function Home() {
   return(<main>
-    <h1>HOME</h1>
-    <Link href="/subroute"><button>ASD</button></Link>
+    <div className="flex bg-slate-600 justify-evenly">
+      <div className=" bg-white shadow-md rounded p-4 m-10 text-center flex-auto float-left clear-both w-[10%] max-w-[20%] justify-center items-center">
+      <Card list={["Rust","Golang","Python","JS/TS"]} />
+      </div>
+      <div className="bg-white shadow-md rounded p-4 m-10 text-center flex-auto float-left clear-both w-[10%] max-w-[20%]">
+      <Card list={["Backend","Frontend","Database","DevOps"]} />
+      </div>
+      </div>
   </main>)
   
 }
